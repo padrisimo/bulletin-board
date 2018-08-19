b47f82429160ac6ca5ba6264a17e12e1ac6715a9
padrisimo <adrianfernandezchamizo@gmail.com>
2018-08-19 03:38:58 +0200

    addding state to note component

---
+++ src/Note.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { FaTrash, FaPencilAlt, FaSave } from 'react-icons/fa';
+
+export default class Note extends Component {
+  state = {
+    editing: false
+  }
+
+  edit = () => {
+    this.setState({editing: true})
+  }
+
+  remove = () => {
+    alert('remove')
+  }
+
+  save = () => {
+    alert('saved');
+  };
+
+  renderForm = () => (
+    <div className="note">
+      <form>
+        <textarea />
+        <button onClick={this.save}><FaSave /></button>
+      </form>
+    </div>
+  );
+
+  renderDisplay = () => {
+    return (
+      <div className="note">
+        <p>note stuff</p>
+        <span>
+          <button onClick={this.remove} id="remove"><FaTrash /></button>
+          <button onClick={this.edit} id="edit"><FaPencilAlt /></button>
+        </span>
+      </div>
+    )
+  }
+
+  render(){
+    return this.state.editing ? this.renderForm : this.renderDisplay;
+  }
+}
